f864bb55a5f52f83cf48c652fe46cd6f5d7105e0
Siddharth Thevaril <siddharththevaril@Siddharths-MacBook-Pro.local>
2020-07-11 02:04:12 +0530

    feat: add asset loader

---
+++ ffc-app/class-register-assets.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Registers plugin assets.
+ *
+ * @package FF_Challenge
+ */
+
+namespace FFCApp;
+
+/**
+ * Defines methods to register | enqueue assets like JS and CSS
+ * necessary for the plugin.
+ */
+class Register_Assets {
+
+	/**
+	 * Calls necessary actions and filters.
+	 */
+	public function init() {
+		add_action( 'wp_enqueue_scripts', array( $this, 'register_scripts' ) );
+	}
+
+	/**
+	 * Registers styles and scripts.
+	 */
+	public function register_scripts() {
+		$is_cached = wp_cache_get( 'ffc_challenge_payload' );
+
+		if ( false === $is_cached ) {
+			wp_register_script( 'ffc-script-front', FFC_URL . 'src/front/js/ffc-front.js', array(), '1.0', true );
+			wp_localize_script( 'ffc-script-front', 'ffcGlobal', array( 'ajaxUrl' => admin_url( 'admin-ajax.php' ), 'ffcShortcodeNonce' => wp_create_nonce( 'ffc-shortcode-nonce' ) ) );
+		}
+	}
+}
