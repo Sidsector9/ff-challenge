c14ee20b2157b03950438e6b1d1186e0b657afd6
Siddharth Thevaril <siddharththevaril@Siddharths-MacBook-Pro.local>
2020-07-11 02:03:55 +0530

    feat: add base loader

---
+++ ffc-app/class-init-app.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Deals with setting up the plugin by instantiating
+ * necessary classes.
+ *
+ * @package FF_Challenge
+ */
+
+namespace FFCApp;
+
+/**
+ * Instantiates classes necessary on plugin initialization.
+ */
+class Init_App {
+
+	/**
+	 * Constructor function.
+	 */
+	public function __construct() {
+		( new \FFCApp\Register_Assets() )->init();
+		( new \FFCApp\Shortcodes\Challenge() )->init();
+		( new \FFCApp\AjaxHandlers\Challenge_Shortcode_Ajax_Handler() )->init();
+	}
+}
