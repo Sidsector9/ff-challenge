e7d7664b0405bb6462986d20b96e187d8c4354e7
Siddharth Thevaril <siddharththevaril@Siddharths-MacBook-Pro.local>
2020-07-11 02:05:21 +0530

    feat: add assets

---
+++ src/front/js/ffc-front.js
@@ -0,0 +1,54 @@
+const ffcShortcodeData = new FormData();
+
+fetch( 'http://api.strategy11.com/wp-json/challenge/v1/1' )
+	.then( response => {
+		return response.json();
+	} )
+	.then( responseData => {
+		const ffcShortcodeData = new FormData();
+		ffcShortcodeData.append( 'action', 'ffc_challenge_shortcode_handler' );
+		ffcShortcodeData.append( 'nonce', ffcGlobal.ffcShortcodeNonce );
+		ffcShortcodeData.append( 'payload', JSON.stringify( responseData ) );
+
+		generate_table( responseData.data );
+
+		fetch( ffcGlobal.ajaxUrl, {
+			method: 'POST',
+			body: ffcShortcodeData,
+		} )
+	} )
+
+function generate_table( tableData ) {
+	const tableHeaderData = tableData.headers;
+	const tableBodyData = tableData.rows;
+	const ffcTable = document.querySelectorAll( '.ffc-challenge-table' );
+
+	[].forEach.call( ffcTable, function( node ) {
+		const fragment = document.createDocumentFragment();
+		const tableHeader = document.createElement( 'thead' );
+		const tableBody = document.createElement( 'tbody' );
+
+		tableHeaderData.forEach( item => {
+			const thNode = document.createElement( 'th' );
+			thNode.innerText = item;
+			tableHeader.appendChild( thNode )
+		} );
+
+		for ( const item in tableBodyData ) {
+			const trNode = document.createElement( 'tr' );
+			trNode.innerHTML = `
+				<td>${ tableBodyData[ item ].id }</td>
+				<td>${ tableBodyData[ item ].fname }</td>
+				<td>${ tableBodyData[ item ].lname }</td>
+				<td>${ tableBodyData[ item ].email }</td>
+				<td>${ tableBodyData[ item ].date }</td>
+			`;
+
+			tableBody.appendChild( trNode );
+		};
+
+		fragment.appendChild( tableHeader );
+		fragment.appendChild( tableBody );
+		node.appendChild( fragment );
+	} )
+}
